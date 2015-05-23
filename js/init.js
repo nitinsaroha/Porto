a449df36907fc6978d9eb991e92eb822564240ff
nitinsaroha <nitinsaroha@outlook.com>
2015-05-23 16:51:43 +0530

    inital commit

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav({
+    	edge: 'right',
+    	closeOnClick: true
+    });
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
